5215cf3181df8172a967f4b11906e48d4633ceca
TauhidNeyaz <asadneyaz5555@gmail.com>
2026-02-02 14:54:42 +0530

    http

--- src/main/scala/lowLevelServer/lowLevelAPI.scala
+++ src/main/scala/lowLevelServer/lowLevelAPI.scala
@@ -3,9 +3,12 @@ package lowLevelServer
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.Http.IncomingConnection
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethod, HttpMethods, HttpRequest, HttpResponse, StatusCode, StatusCodes, Uri}
+import akka.http.scaladsl.server.ContentNegotiator.Alternative.ContentType
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.util.{Failure, Success}
@@ -24,9 +27,93 @@ object lowLevelAPI extends App {
 
   private val serverBindingFuture = serverSource.to(connectionSink).run()
   serverBindingFuture.onComplete {
-    case Success(_) =>
+    case Success(binding) =>
       println("Server binding successful.")
+      binding.terminate(2 seconds)
     case Failure(ex) => println(s"Server binding failed: $ex")
   }
 
+  /*
+      Methods 1. synchronously serve HTTP response
+   */
+
+  private val requestHandler : HttpRequest => HttpResponse = {
+    case HttpRequest(HttpMethods.GET, _, _, _, _) =>
+      HttpResponse(
+        StatusCodes.OK,
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            | <body>
+            |   Hello from Akka HTTP!
+            | </body>
+            |</html>
+            |""".stripMargin
+        )
+      )
+
+    case request: HttpRequest =>
+      request.discardEntityBytes()
+      HttpResponse(
+        StatusCodes.NotFound,
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            | <body>
+            |   The resource can't be found
+            | </body>
+            |</html>
+            |""".stripMargin
+        )
+      )
+  }
+
+  private val httpSyncConnectionHandler= Sink.foreach[IncomingConnection] { connection =>
+    connection.handleWithSyncHandler(requestHandler)
+  }
+  Http().bind("localhost", 8080).runWith(httpSyncConnectionHandler)
+
+  /*
+        Methods 2. Asynchronously serve HTTP response
+   */
+
+  private val AsyncrequestHandler : HttpRequest => Future[HttpResponse] = {
+    case HttpRequest(HttpMethods.GET, Uri.Path("/home"), _, _, _) =>
+      Future(HttpResponse(
+        StatusCodes.OK,
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            | <body>
+            |   Hello from Akka HTTP!
+            | </body>
+            |</html>
+            |""".stripMargin
+        )
+      ))
+
+    case request: HttpRequest =>
+      request.discardEntityBytes()
+      Future(HttpResponse(
+        StatusCodes.NotFound,
+        entity = HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          """
+            |<html>
+            | <body>
+            |   The resource can't be found
+            | </body>
+            |</html>
+            |""".stripMargin
+        )
+      ))
+  }
+
+  private val httpAsyncConnectionHandler= Sink.foreach[IncomingConnection] { connection =>
+    connection.handleWithAsyncHandler(AsyncrequestHandler)
+  }
+  Http().bind("localhost", 8081).runWith(httpAsyncConnectionHandler)
 }
